ade47d06062b44f2740ee9dfe2ec228b9eb5af7a
Mohammad Mahdi Bahrami <90021059+gikdev@users.noreply.github.com>
2024-04-01 08:44:30 +0330

    Simplified the logic side
    Shrunk 26 lines of code into 11 lines!

--- src/script.js
+++ src/script.js
@@ -2,25 +2,10 @@ const html = document.querySelector('html')
 const translations = document.querySelectorAll('.persian')
 const hideTranslation = document.querySelector('#hide-translation')
 const darkTheme = document.querySelector('#dark-theme')
-const settings = {
-  hideTranslation: false,
-  darkTheme: false,
-}
 
-document.addEventListener('DOMContentLoaded', () => {
-  hideTranslation.addEventListener('change', e => {
-    settings.hideTranslation = e.target.checked
-    apply(settings)
-  })
-  darkTheme.addEventListener('change', e => {
-    settings.darkTheme = e.target.checked
-    apply(settings)
-  })
-})
-
-function apply({ hideTranslation: ht, darkTheme: dt}) {
-  if (ht) translations.forEach(t => t.style.display = 'none')
-  else    translations.forEach(t => t.style.display = 'block')
-  if (dt) html.classList.add('dark')
-  else    html.classList.remove('dark')
-}
+darkTheme.addEventListener('change', () => html.classList.toggle('dark'))
+hideTranslation.addEventListener('change', e =>
+  translations.forEach(t =>
+    t.style.display = (e.target.checked) ? 'none' : 'block'
+  )
+)
